59e2f54b87d637cb0f89621ae4f473126669f68b
DanZeta1 <fabrizela067@gmail.com>
2026-04-08 22:26:40 -0600

    Detalles

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,32 @@
 import { defineConfig } from 'vite'
 import purgecss from 'vite-plugin-purgecss'
+import handlebars from 'vite-plugin-handlebars'
 
 export default defineConfig({
-  // Cambia 'PW2OPVDRYWALL' por el nombre exacto de tu repositorio en GitHub
   base: '/PW2OPVDRYWALL/',
 
   build: {
     cssMinify: true,
     rollupOptions: {
       input: {
-        main:              'index.html',
-        acercaDe:          'html/acercaDe.html',
-        contactenos:       'html/contactenos.html',
-        formRegistro:      'html/formRegistro.html',
-        galeria:           'html/galeria.html',
-        nuestrosProgramas: 'html/nuestrosProgramas.html',
-        porqueOpv:         'html/porqueOpv.html',
-        preguntasFrecuentes: 'html/preguntasFrecuentes.html',
+        main:                 'index.html',
+        acercaDe:             'html/acercaDe.html',
+        contactenos:          'html/contactenos.html',
+        formRegistro:         'html/formRegistro.html',
+        galeria:              'html/galeria.html',
+        nuestrosProgramas:    'html/nuestrosProgramas.html',
+        porqueOpv:            'html/porqueOpv.html',
+        preguntasFrecuentes:  'html/preguntasFrecuentes.html',
         terminosYcondiciones: 'html/terminosYcondiciones.html',
-        testimonios:       'html/testimonios.html',
+        testimonios:          'html/testimonios.html',
       }
     }
   },
 
   plugins: [
+    handlebars({
+      partialDirectory: './partials',
+    }),
     purgecss({
       content: [
         './index.html',
